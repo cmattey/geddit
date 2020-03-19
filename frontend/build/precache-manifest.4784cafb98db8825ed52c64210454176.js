4309c64e7cd207628e91bbb92e7ce9619d7a2404
cmattey <cmattey@dons.usfca.edu>
2020-03-18 21:58:32 -0700

    commited frontend ui build folder, to allow access on heroku for deployment

---
+++ frontend/build/precache-manifest.4784cafb98db8825ed52c64210454176.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a56027c9bf6c036d5b3e7128a193c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdeae1482267f5f5a4bf",
+    "url": "/static/js/2.71f513f5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.71f513f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1674244c3a9b56de32ee",
+    "url": "/static/js/main.db652bb0.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
